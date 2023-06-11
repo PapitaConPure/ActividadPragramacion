0f5bcefe624b6bfaa1b630ba0d49a83995ad0e53
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2023-06-10 22:01:00 -0300

    #2

--- DIOS/Promedio.cs
+++ DIOS/Promedio.cs
@@ -2,24 +2,55 @@
 
 namespace DIOS {
     class Promedio {
-        int acumulado;
-        int cantidadValores;
+        int[] valores;
+		int cantidadValores;
 
         public Promedio() { 
-            acumulado = 0;
-            cantidadValores = 0;
-        }
+            this.valores = new int[2];
+			this.cantidadValores = 0;
+		}
 
-        public void AgregarValor(int num) {
-            acumulado += num;
-            cantidadValores++;
-        }
+		private int Acumulado {
+			get {
+				int acumulado = 0;
 
-        public double CalcularPromedio() { 
-            if(cantidadValores == 0)
+				int i = 0;
+				while(i < cantidadValores)
+					acumulado += valores[i++];
+
+				return acumulado;
+			}
+		}
+
+		public void AgregarValor(int num) {
+			this.valores[this.cantidadValores] = num;
+			this.cantidadValores++;
+			this.EscalarArreglo();
+		}
+
+		public double CalcularPromedio() {
+            if(this.cantidadValores == 0)
                 return 0;
-            else
-                return Convert.ToDouble(acumulado) / cantidadValores;
+
+			return Convert.ToDouble(this.Acumulado) / this.cantidadValores;
         }
+
+		private void EscalarArreglo() {
+			int nuevoLargo = this.valores.Length;
+
+			if(this.cantidadValores < this.valores.Length / 2)
+				nuevoLargo /= 2;
+			else if(this.cantidadValores >= this.valores.Length)
+				nuevoLargo *= 2;
+			else
+				return;
+
+			int[] nuevoArr = new int[nuevoLargo];
+
+			for(int i = 0; i < this.valores.Length; i++)
+				nuevoArr[i] = this.valores[i];
+
+			this.valores = nuevoArr;
+		}
     }
 }
